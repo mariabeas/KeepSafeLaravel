9a96f9ad7625e803c710436ccea75d43915e3623
mariabeas <mdm_beas@hotmail.com>
2016-04-19 20:06:29 +0200

    laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Bienvenido</div>
+
+                <div class="panel-body">
+                    
+                    <a href="http://es.tinypic.com?ref=30k5a12" target="_blank"><img src="http://i64.tinypic.com/30k5a12.png" border="0" alt="Image and video hosting by TinyPic"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
